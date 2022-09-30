4981a68bcb52e7d43578ccef11b745fe7c99dcc1
Matt Dubbert <mdubbert+gh@gmail.com>
2022-09-30 16:11:47 -0600

    Refactor into loop with randomly selected records

--- src/python/producer/producer.py
+++ src/python/producer/producer.py
@@ -1,11 +1,11 @@
 import boto3
 import json
 import logging
-import os
+# import os
+import random
 import requests
 
 from datetime import datetime
-# from decimal import Decimal
 from uuid import uuid4
 
 logging.basicConfig(level=logging.INFO)
@@ -18,51 +18,60 @@ stream_name = 'astro-stream'
 
 # JPL Small-Body Database
 SBDB_QUERY_URL = 'https://ssd-api.jpl.nasa.gov/sbdb_query.api'
-SBDB_QUERY_FIELDS = 'spkid,full_name,GM,kind,neo'
-SBDB_QUERY_LIMIT = 5
+SBDB_QUERY_FIELDS = 'spkid,full_name,GM,kind,neo,pha'
+SBDB_RECORDS_PER_QUERY = 500
+SBDB_BATCHES_PER_RUN = 8
 
+STRING_TO_BOOLEAN_MAP = {
+    'Y': True,
+    'N': False
+}
 
 def main():
     session = boto3.session.Session()
     kinesis_client = session.client('kinesis', region_name=region)
 
-    records = get_source_data()
-    formatted_records = format_payload(records)
-    failed_count = send_to_kinesis(kinesis_client, formatted_records)
+    batch_count = 0
+    starting_record_position = random.randrange(0, 125000, 1) 
 
-    if failed_count > 0:
-        raise EnvironmentError(f'Failed to send {failed_count} records to stream')
+    while batch_count < SBDB_BATCHES_PER_RUN:
+        records = get_source_data(starting_record_position)
+        formatted_records = format_payload(records)
+        send_to_kinesis(kinesis_client, formatted_records)
+        batch_count += 1
+        starting_record_position += SBDB_RECORDS_PER_QUERY
 
-    logging.info(f'Successfuly pushed {len(formatted_records)} records to stream')        
 
-
-def get_source_data():
-    logging.info('Requesting source data')
+def get_source_data(starting_position=0) -> dict:
+    logging.info(f'Requesting source data: {starting_position}-{starting_position + SBDB_RECORDS_PER_QUERY}')
     query_params = {
         'fields': SBDB_QUERY_FIELDS,
-        'limit': SBDB_QUERY_LIMIT
+        'limit': SBDB_RECORDS_PER_QUERY,
+        'limit-from': starting_position,
+        'sb-kind': 'a'                     # asteroids
+        # 'sb-group': 'pha'                # potentially hazardous asteroids. seems sparse.
     }
 
     response = requests.get(SBDB_QUERY_URL, params=query_params)
     response.raise_for_status()
     return response.json()
 
-def format_payload(records):
+def format_payload(records) -> list:
     formatted_records = []
     current_timestamp = datetime.now().isoformat()
 
     # TODO: dynamically assign field names
 
     for record in records['data']:
-        spk_id, full_name, gravitational_mass, kind, neo = record
+        spk_id, full_name, gravitational_mass, kind, neo, pha = record
 
         trimmed = {
             'spk_id': spk_id,
             'full_name': full_name.strip(),
-            # 'gravitational_mass': Decimal(gravitational_mass),
             'gravitational_mass': gravitational_mass,
             'kind': kind,
             'is_neo': to_boolean(neo),
+            'is_pha': to_boolean(pha),
             'processing_timestamp': current_timestamp
         }
 
@@ -77,22 +86,28 @@ def format_payload(records):
 
 
 def send_to_kinesis(client, record_list) -> int:
+    if len(record_list) == 0:
+        logging.warning('No records to send')
+        return
+
     response = client.put_records(
         Records=record_list,
         StreamName=stream_name
     )
 
     # TODO: better response checking / logging
+    failed_count = response.get('FailedRecordCount', -1)
 
-    return response.get('FailedRecordCount', -1)
+    if failed_count > 0:
+        raise EnvironmentError(f'Failed to send {failed_count} records to stream')
+
+    logging.info(f'Successfully pushed {len(record_list)} records to stream')  
 
 def to_boolean(input_str: str) -> bool:
-    if input_str == 'Y':
-        return True
-    elif input_str == 'N':
-        return False
-    
-    raise NotImplementedError('Unexpected input value')
+    try:
+        return STRING_TO_BOOLEAN_MAP[input_str]
+    except KeyError:
+        raise NotImplementedError(f'Unexpected input value. Expecting one of {STRING_TO_BOOLEAN_MAP.keys()}')
 
 if __name__ == "__main__":
     main()
